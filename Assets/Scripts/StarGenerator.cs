0b2cc151da30a81556bb77f79f5c9344fbcfa297
Alec Hoffmann <astrohoff@ufl.edu>
2017-01-21 02:47:42 -0500

    Random stars.

---
+++ Assets/Scripts/StarGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarGenerator : MonoBehaviour {
+    private Mesh starMesh;
+
+	// Use this for initialization
+	void Awake () {
+        // Add "Mesh" components to object.
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+        MeshRenderer meshRend = gameObject.AddComponent<MeshRenderer>();
+        // Create empty mesh.
+        starMesh = new Mesh();
+        meshFilter.mesh = starMesh;
+        // Add white material.
+        Material mat = new Material(Shader.Find("Unlit/Color"));
+        mat.color = Color.white;
+        meshRend.material = mat;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GenerateStars(Vector3[] positions, Color[] colors, float distance)
+    {
+        Debug.Log("Generating stars");
+        // Set stars to given distance.
+        int[] indices = new int[positions.Length];
+        for(int i = 0; i < positions.Length; i++)
+        {
+            positions[i] = positions[i].normalized * distance;
+            indices[i] = i;
+        }
+        // Set mesh attributes.
+        starMesh.vertices = positions;
+        starMesh.colors = colors;
+        starMesh.SetIndices(indices, MeshTopology.Points, 0);
+        // Update the mesh rendering bounds.
+        starMesh.RecalculateBounds();
+    }
+
+}
---
+++ Assets/Scripts/StarInfoGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarInfoGenerator : MonoBehaviour {
+    public int starCount = 1000;
+    public float distance = 100;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Generating star info");
+        Vector3[] positions = new Vector3[starCount];
+        Color[] colors = new Color[starCount];
+        for(int i = 0; i < starCount; i++)
+        {
+            positions[i] = Random.onUnitSphere * distance;
+            colors[i] = new Color(Random.Range(0, 255), Random.Range(0, 255), Random.Range(0, 255));
+        }
+        GetComponent<StarGenerator>().GenerateStars(positions, colors, distance);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
